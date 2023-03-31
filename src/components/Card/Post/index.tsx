4ea6147fcb007b90c84c20a82082fb00ad4e7c06
wirayuda <wirayuda233@gmail.com>
2023-03-31 07:59:29 -0700

    some changes on css

--- src/components/Card/Post/index.tsx
+++ src/components/Card/Post/index.tsx
@@ -8,8 +8,6 @@ import { PostComment } from './Comments';
 import { db } from '@/config/firebase';
 import { doc, onSnapshot } from 'firebase/firestore';
 import { useSession } from 'next-auth/react';
-import { rgbDataURL } from '@/util/colorPicker';
-
 export interface ICommentsProps {
 	comment: string;
 	commentByName: string;
@@ -34,24 +32,7 @@ export default function PostCard({ post, followingLists }: IPostCardProps) {
 		});
 		return () => unsub();
 	}, [post.postId]);
-	const shimmer = (w: number, h: number) => `
-<svg width="${w}" height="${h}" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-  <defs>
-    <linearGradient id="g">
-      <stop stop-color="#333" offset="20%" />
-      <stop stop-color="#222" offset="50%" />
-      <stop stop-color="#333" offset="70%" />
-    </linearGradient>
-  </defs>
-  <rect width="${w}" height="${h}" fill="#333" />
-  <rect id="r" width="${w}" height="${h}" fill="url(#g)" />
-  <animate xlink:href="#r" attributeName="x" from="-${w}" to="${w}" dur="1s" repeatCount="indefinite"  />
-</svg>`;
 
-	const toBase64 = (str: string) =>
-		typeof window === 'undefined'
-			? Buffer.from(str).toString('base64')
-			: window.btoa(str);
 
 	return (
 		<div className='w-full my-2'>
@@ -66,11 +47,11 @@ export default function PostCard({ post, followingLists }: IPostCardProps) {
 					src={post?.image}
 					width={500}
 					height={500}
-					sizes='100vw'
+					sizes='(max-width: 500px) 100vw, 500px'
 					priority
 					quality={55}
 					placeholder='blur'
-					blurDataURL={`data:image/svg+xml;base64,${toBase64(shimmer(700, 475))}`}
+					blurDataURL={post.image}
 					referrerPolicy='no-referrer'
 					security='restricted'
 					className=' object-cover w-full h-auto'
