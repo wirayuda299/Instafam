deabd169f760c81ba2603ef5d3c0fbdc4322e200
wirayuda <wirayuda233@gmail.com>
2023-04-01 08:58:41 -0700

    create messages ui layout

--- src/pages/messages.tsx
+++ src/pages/messages.tsx
@@ -181,7 +181,7 @@ export default function Messages() {
 													A
 												</div>
 												<div className='relative mr-3 text-sm bg-indigo-100 py-2 px-4 shadow rounded-xl'>
-													<div>I'm ok what about you?</div>
+													<div>Im ok what about you?</div>
 												</div>
 											</div>
 										</div>
