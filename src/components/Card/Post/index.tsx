1d10bafbebc474f656d9be7cc11bbf75364453e2
wirayuda <wirayuda233@gmail.com>
2023-04-14 01:11:16 -0700

    fix zod error

--- src/components/Card/Post/index.tsx
+++ src/components/Card/Post/index.tsx
@@ -39,7 +39,6 @@ function PostCard({ post, ssr, session, isr }: IPostCardProps) {
 	const { replace, asPath } = useRouter();
 	const refreshData = () => replace(asPath);
 	const [isMenuOpen, setIsMenuOpen] = useState<boolean>(false);
-	
 
 	useEffect(() => {
 		const unsub = onSnapshot(doc(db, 'posts', `post-${post.postId}`), (doc) => {
@@ -68,7 +67,7 @@ function PostCard({ post, ssr, session, isr }: IPostCardProps) {
 		return () => unsub();
 	}, [db, post]);
 
-	const isValid = PostCardSchema.parse({ post, ssr, session });
+	const isValid = PostCardSchema.parse({ post, ssr, session, isr });
 	if (!isValid) throw new Error('Invalid Props for PostCard Component');
 
 	return (
@@ -90,13 +89,15 @@ function PostCard({ post, ssr, session, isr }: IPostCardProps) {
 						'data:image/jpeg;base64,/9j/4AAQSkZJRgABAQAAAQABAAD/2wBDAP//////////////////////////////////////////////////////////////////////////////////////2wBDAf//////////////////////////////////////////////////////////////////////////////////////wAARCAACAAMDASIAAhEBAxEB/8QAHwAAAQUBAQEBAQEAAAAAAAAAAAECAwQFBgcICQoL/8QAtRAAAgEDAwIEAwUFBAQAAAF9AQIDAAQRBRIhMUEGE1FhByJxFDKBkaEII0KxwRVS0fAkM2JyggkKFhcYGRolJicoKSo0NTY3ODk6Q0RFRkdISUpTVFVWV1hZWmNkZWZnaGlqc3R1dnd4eXqDhIWGh4iJipKTlJWWl5iZmqKjpKWmp6ipqrKztLW2t7i5usLDxMXGx8jJytLT1NXW19jZ2uHi4+Tl5ufo6erx8vP09fb3+Pn6/8QAHwEAAwEBAQEBAQEBAQAAAAAAAAECAwQFBgcICQoL/8QAtREAAgECBAQDBAcFBAQAAQJ3AAECAxEEBSExBhJBUQdhcRMiMoEIFEKRobHBCSMzUvAVYnLRChYkNOEl8RcYGRomJygpKjU2Nzg5OkNERUZHSElKU1RVVldYWVpjZGVmZ2hpanN0dXZ3eHl6goOEhYaHiImKkpOUlZaXmJmaoqOkpaanqKmqsrO0tba3uLm6wsPExcbHyMnK0tPU1dbX2Nna4uPk5ebn6Onq8vP09fb3+Pn6/9oADAMBAAIRAxEAPwBaKKKAP//Z'
 					}
 					quality={60}
-					loader={() => imageLoader({src: post?.image, width: 1300, quality: 10})}
+					loader={() =>
+						imageLoader({ src: post?.image, width: 1300, quality: 10 })
+					}
 					priority
 					className='object-cover w-full h-auto rounded-lg'
 					alt={post?.author ?? 'user post image'}
 				/>
 				<ActionButton
-				isr
+					isr={isr}
 					ssr={ssr}
 					refreshData={refreshData}
 					savedPosts={savedPosts}
@@ -127,7 +128,7 @@ function PostCard({ post, ssr, session, isr }: IPostCardProps) {
 					ssr={ssr}
 					users={users}
 				/>
-				<ReportModal session={session}/>
+				<ReportModal session={session} />
 			</div>
 		</div>
 	);
