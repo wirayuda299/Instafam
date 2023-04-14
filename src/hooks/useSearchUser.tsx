a63c4610040dd051da7b51186c28baa7536b56ef
wirayuda <wirayuda233@gmail.com>
2023-04-14 10:22:03 -0700

    create api route

--- src/hooks/useSearchUser.tsx
+++ src/hooks/useSearchUser.tsx
@@ -1,8 +1,6 @@
 import { resultsState } from "@/store/results";
-import { useSession } from "next-auth/react";
-import { useRouter } from "next/router";
 import { useTransition } from "react";
-import { useForm } from "react-hook-form";
+import { FieldValues, useForm } from "react-hook-form";
 import { toast } from "react-hot-toast";
 import { useRecoilState } from "recoil";
 
@@ -10,55 +8,26 @@ export default function useSearchUser() {
   const { register, handleSubmit, resetField } = useForm();
   const [results, setResults] = useRecoilState(resultsState);
   const [isPending, startTransition] = useTransition();
-  const { data: session } = useSession();
-  const router = useRouter();
 
-  const onSubmit = async (data: any) => {
+  const onSubmit = async (data: FieldValues) => {
     resetField("search");
     try {
-      if (!session) {
-        toast.error("You must be logged in to search for users");
-        router.push("/auth/signin");
-        setResults([]);
-        return;
+      const response = await fetch(`api/search-user?search=${data.search}`, {
+        method: "GET",
+        referrerPolicy: "no-referrer",
+        signal: new AbortController().signal,
+        cache: "no-cache",
+      })
+      const result = await response.json()
+      if (result) {
+        console.log(result);
+        
+        startTransition(() => {
+          setResults(result);
+        });
       }
-      // const reg = /^@[A-Za-z0-9]+$/g;
-      // if (!reg.test(data.search)) {
-      // 	toast.error('Invalid characters in search field');
-      // 	setResults([]);
-      // 	return;
-      // }
-
-      const { db } = await import("@/config/firebase");
-      const { query, collection, getDocs, where } = await import(
-        "firebase/firestore"
-      );
-      const q = query(
-        collection(db, "users"),
-        where("username", "==", data.search)
-      );
-      const nameQueries = query(
-        collection(db, "users"),
-        where("name", "==", data.search)
-      );
-
-      const res = await getDocs(q);
-      if (res.docs.length < 1) {
-        const nameRes = await getDocs(nameQueries);
-        if (!isPending) {
-          startTransition(() => {
-            setResults(
-              nameRes.docs.map((doc) => ({ ...doc.data(), id: doc.id }))
-            );
-          });
-        }
-        return;
-      }
-      startTransition(() => {
-        setResults(res.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
-      });
     } catch (error: any) {
-      console.log(error.message);
+      toast.error(error.message);
     }
   };
   return {
