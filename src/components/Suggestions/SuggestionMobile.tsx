c2c6805cb5956af67c799f0e5b1475a3d8db07d4
wirayuda <wirayuda233@gmail.com>
2023-04-29 02:49:10 -0700

    hide overflow text

--- src/components/Suggestions/SuggestionMobile.tsx
+++ src/components/Suggestions/SuggestionMobile.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link'
 export default function SuggestionMobile({user}: {user:IUser}) {
 
   return (
-    <div className="w-44 h-44 flex flex-col bg-white rounded-lg shadow-md border p-5 justify-center items-center" >
+    <div className="w-44 h-44 flex flex-col bg-white rounded-lg shadow-md border p-5 justify-center items-center relative" >
       <Image
         src={user.image}
         width={50}
@@ -14,10 +14,10 @@ export default function SuggestionMobile({user}: {user:IUser}) {
         placeholder='blur'
         blurDataURL={user.image}
         alt={user.username} />
-      <h2 className="font-bold mt-2">
+      <h2 className="font-bold mt-2 truncate w-full" title={user.username}>
         {user.username}
       </h2>
-      <Link className="bg-blue-500 rounded-md w-full py-1 text-center text-white mt-2" href={`/profile/${user.username}`}>
+      <Link className="bg-blue-500 w-32 h-7 rounded-md  text-center text-white mt-2 " href={`/profile/${user.username}`}>
         View Profile
       </Link>
 
