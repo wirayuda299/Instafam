6e61bc19c1d951127bd2d61fc5a43dc50aa707f7
wirayuda <wirayuda233@gmail.com>
2023-05-03 20:22:42 -0700

    switch to btn for extra menu list instead a link

--- src/components/Navigation/ExtraMenus.tsx
+++ src/components/Navigation/ExtraMenus.tsx
@@ -55,66 +55,42 @@ export default function ExtraMenus() {
     <>
       {isExtraListOpen ? (
         <div
-          className={` relative w-full flex-col justify-center space-y-1 lg:space-y-3  ${
-            isExtraListOpen ? "flex animate-fadeIn" : "hidden animate-fadeOut"
-          }`}
+          className={` relative w-full flex-col justify-center space-y-1 lg:space-y-3  ${isExtraListOpen ? "flex animate-fadeIn" : "hidden animate-fadeOut"
+            }`}
         >
           <div
-            className={`md:bg-opacity-85  ${
-              darkMode ? "bg-black text-white" : "!bg-white text-black"
-            }  -left-0 w-full rounded-md py-4 dark:bg-opacity-95  sm:w-44 md:-top-[120px] md:w-60 lg:-top-[140px]  ${
-              isExtraListOpen ? " absolute  z-[999] block " : "hidden"
-            }`}
+            className={`md:bg-opacity-85  ${darkMode ? "bg-black text-white" : "!bg-white text-black"
+              }  -left-0 w-full rounded-md py-4 dark:bg-opacity-95  sm:w-44 md:-top-[120px] md:w-60 lg:-top-[140px]  ${isExtraListOpen ? " absolute  z-[999] block " : "hidden"
+              }`}
           >
             <ul className="w-full px-2">
               {ExtraMenusLists.map((list) => (
                 <li
                   key={list.id}
-                  className={`ease w-fit truncate rounded-full  border-b  px-5 py-2 transition-all duration-300   dark:border-b-0  md:w-full md:py-3 ${
-                    darkMode
+                  className={`ease w-fit truncate rounded-full  border-b  px-5 py-2 transition-all duration-300   dark:border-b-0  md:w-full md:py-3 ${darkMode
                       ? "border-none text-white hover:bg-[#b9b9b917]"
                       : "hover:bg-gray-200"
-                  } ${
-                    darkMode ? "bg-black text-white" : "bg-white text-black"
-                  }`}
+                    } ${darkMode ? "bg-black text-white" : "bg-white text-black"
+                    }`}
                   title={list.title}
                   onClick={list.event}
                 >
-                  {list.id === 3 ? (
-                    <Link
-                      href={"/activity"}
-                      className="flex w-full items-center justify-between gap-2 space-x-2"
-                    >
-                      <span className="text-sm font-semibold md:text-base md:font-medium">
-                        {list.title}
-                      </span>
-                      <span
-                        className={`text-2xl ${
-                          darkMode ? "text-white" : "text-black"
+                  <button
+                    className="flex w-full items-center justify-between gap-2 space-x-2"
+                    type="button"
+                    name={list.title}
+                    title={list.title}
+                  >
+                    <span className="text-sm font-semibold md:text-base md:font-medium">
+                      {list.title}
+                    </span>
+                    <span
+                      className={`text-2xl ${darkMode ? "text-white" : "text-black"
                         }`}
-                      >
-                        {list.icon}
-                      </span>
-                    </Link>
-                  ) : (
-                    <button
-                      className="flex w-full items-center justify-between gap-2 space-x-2"
-                      type="button"
-                      name={list.title}
-                      title={list.title}
                     >
-                      <span className="text-sm font-semibold md:text-base md:font-medium">
-                        {list.title}
-                      </span>
-                      <span
-                        className={`text-2xl ${
-                          darkMode ? "text-white" : "text-black"
-                        }`}
-                      >
-                        {list.icon}
-                      </span>
-                    </button>
-                  )}
+                      {list.icon}
+                    </span>
+                  </button>
                 </li>
               ))}
             </ul>
